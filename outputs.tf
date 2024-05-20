33dd7bc478998ab084c6b222ac83707599f9addb
José Manuel Ortega Falcón <jortfal@users.noreply.github.com>
2024-05-20 15:30:13 +0000

    fix: list items should be accessed using square brackets (terraform_deprecated_index)

--- outputs.tf
+++ outputs.tf
@@ -16,5 +16,5 @@ limitations under the License.
 
 output "id" {
   description = "The ID of the Resource Group"
-  value       = azurerm_resource_group.this.*.id
+  value       = azurerm_resource_group.this[*].id
 }
